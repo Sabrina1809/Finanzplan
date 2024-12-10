a24047e15b86019d8a0ad4c0a7d57de4c98e629a
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2024-12-10 21:30:17 +0100

    Korrekturen

--- js/loadingSpinner.js
+++ js/loadingSpinner.js
@@ -20,12 +20,13 @@ function showDescription() {
 
 function saveAcceptButton() {
     loadingScreenOption = 1;
-    console.log(loadingScreenOption);
+    // console.log(loadingScreenOption);
     localStorage.setItem("loadingScreenFinanzplan", JSON.stringify(loadingScreenOption));
     document.querySelector(".accept_button_descr_ctn").classList.remove("animation_visible");
     document.querySelector(".accept_button_descr_ctn").classList.add("animation_hidden");
     document.querySelector(".accept_button_descr_ctn").style.opacity = "0";  
     setTimeout(() => {document.querySelector(".loading_img_ctn").classList.add("animation_after_click")},1500)
     setTimeout(() => {document.querySelector(".loading_spinner_ctn").classList.add("animation_hide_loading_ctn")}, 3500)
-    setTimeout(() => {document.querySelector(".loading_spinner_ctn").style.display = "none"}, 4400)
+    setTimeout(() => {document.querySelector(".loading_spinner_ctn").style.display = "none"}, 3500)
+    setTimeout(() => {document.querySelector(".loading_img_ctn").style.display = "none"}, 3500)
 }
